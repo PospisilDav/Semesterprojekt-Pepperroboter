e5b609d288771c107303518116d822e2f561cec1
Polarcrafter2 <denisrehberg@gmail.com>
2025-04-28 12:38:42 +0200

    Refactor settings page scripts to improve mute functionality and streamline event handling

--- html/newDesign/scripts/settings.js
+++ html/newDesign/scripts/settings.js
@@ -3,11 +3,39 @@ var isMuted = false;
 function toggleMute() {
     if (isMuted) {
         window.pepperController.setUnmute();
-        document.getElementById("muteText").textContent = "Mute";
+        window.pepperController.playSound(0);
+        document.getElementById("muteText").textContent = "Stummschalten";
     } else {
         window.pepperController.setMute();
-        document.getElementById("muteText").textContent = "Unmute";
+        window.pepperController.playSound(0);
+        document.getElementById("muteText").textContent = "Entstummen";
     }
 
     isMuted = !isMuted;
 }
+
+function volPlus() {
+    window.pepperController.volPlus();
+    window.pepperController.playSound(0);
+}
+
+function volMinus() {
+    window.pepperController.volMinus();
+    window.pepperController.playSound(0);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("muteBtn").addEventListener("click", function () {
+        if (isMuted) {
+            window.pepperController.setUnmute();
+            window.pepperController.playSound(0);
+            document.getElementById("muteText").textContent = "Stummschalten";
+        } else {
+            window.pepperController.setMute();
+            window.pepperController.playSound(0);
+            document.getElementById("muteText").textContent = "Entstummen";
+        }
+
+        isMuted = !isMuted;
+    });
+});
