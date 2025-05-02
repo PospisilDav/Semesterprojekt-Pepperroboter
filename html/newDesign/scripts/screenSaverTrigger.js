2f821d8f58cc0162927a73bbf6f95f43eb75ec0d
David Pospisil <pospisil.david.29@gmail.com>
2025-05-02 15:05:17 +0200

    Tried to fix screenSaverTrigger

--- html/newDesign/scripts/screenSaverTrigger.js
+++ html/newDesign/scripts/screenSaverTrigger.js
@@ -1,29 +1,77 @@
-const INACTIVITY_TRIGGER_SECONDS = 120;
-const TIMEOUT_POLLING_INTERVAL = 1000;
+var INACTIVITY_TRIGGER_SECONDS = 10;
+var TIMEOUT_POLLING_INTERVAL = 1000;
 
 function resolve_path_dynamically() {
-  const currentPath = window.location.pathname;
-  const pathParts = currentPath.split("/");
-  const projectRootIndex = pathParts.indexOf("newDesign");
-  const rootPath = pathParts.slice(0, projectRootIndex + 1).join("/");
-  const htmlPath = "file://" + rootPath + "/pages/screenSaver/ScreenSaver.html";
+  var currentPath = window.location.pathname;
+  var pathParts = currentPath.split("/");
+  var projectRootIndex = -1;
+  
+  for (var i = 0; i < pathParts.length; i++) {
+    if (pathParts[i] === "newDesign") {
+      projectRootIndex = i;
+      break;
+    }
+  }
+  
+  if (projectRootIndex === -1) {
+    console.log("Warning: Could not locate project root directory");
+    return "ScreenSaver.html";
+  }
+  
+  var rootPath = pathParts.slice(0, projectRootIndex + 1).join("/");
+  var htmlPath = "file://" + rootPath + "/pages/screenSaver/ScreenSaver.html";
   return htmlPath;
 }
 
 var last_interaction = Date.now();
+var inactivityTimer = null;
 
-setInterval(function () {
-  const now = Date.now();
-  const seconds_since_last_interaction = Math.floor(
-    (now - last_interaction) / 1000
-  );
-  if (seconds_since_last_interaction >= INACTIVITY_TRIGGER_SECONDS) {
-    console.log("Inactivity detected, redirecting to screen saver...");
-    window.location.href = resolve_path_dynamically();
+function initScreenSaverTrigger() {
+  inactivityTimer = setInterval(function() {
+    var now = Date.now();
+    var seconds_since_last_interaction = Math.floor(
+      (now - last_interaction) / 1000
+    );
+    
+    if (seconds_since_last_interaction >= INACTIVITY_TRIGGER_SECONDS) {
+      console.log("Inactivity detected, redirecting to screen saver...");
+      try {
+        window.location.href = resolve_path_dynamically();
+      } catch (e) {
+        console.log("Error redirecting to screen saver: " + e.message);
+      }
+    }
+  }, TIMEOUT_POLLING_INTERVAL);
+  
+  addInteractionEvents();
+}
+
+function addInteractionEvents() {
+  var events = ["mousedown", "mousemove", "keypress", "scroll", "touchstart"];
+  
+  for (var i = 0; i < events.length; i++) {
+    if (document.addEventListener) {
+      document.addEventListener(events[i], resetInteractionTimer, false);
+    } else if (document.attachEvent) {
+      document.attachEvent("on" + events[i], resetInteractionTimer);
+    } else {
+      document["on" + events[i]] = resetInteractionTimer;
+    }
   }
-}, TIMEOUT_POLLING_INTERVAL);
+}
 
-document.addEventListener("click", function () {
-  console.log("User interaction detected, resetting inactivity timer...");
+function resetInteractionTimer() {
   last_interaction = Date.now();
-});
+}
+
+if (window.addEventListener) {
+  window.addEventListener("load", initScreenSaverTrigger, false);
+} else if (window.attachEvent) {
+  window.attachEvent("onload", initScreenSaverTrigger);
+} else {
+  window.onload = initScreenSaverTrigger;
+}
+
+if (document.readyState === "complete" || document.readyState === "interactive") {
+  setTimeout(initScreenSaverTrigger, 1);
+}
