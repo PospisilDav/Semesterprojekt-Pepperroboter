f8a865a98ca1429625a4b69c6428eaca4d99c3ca
Polarcrafter2 <denisrehberg@gmail.com>
2025-05-02 14:43:37 +0200

    update screen saver trigger logic to increase inactivity timeout; enhance mute functionality with visual feedback in settings.

--- html/newDesign/components/speedDial/speedDial.js
+++ html/newDesign/components/speedDial/speedDial.js
@@ -1,10 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const speedDial = document.querySelector('.speed-dial');
-    const speedDialButton = document.querySelector('.speed-dial-button');
-    
-    if (speedDialButton) {
-        speedDialButton.addEventListener('click', function() {
-            speedDial.classList.toggle('active');
-        });
-    }
+var isMuted = false;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const speedDial = document.querySelector(".speed-dial");
+  const speedDialButton = document.querySelector(".speed-dial-button");
+  const muteBtn = document.getElementById("muteBtn");
+
+  if (speedDialButton) {
+    speedDialButton.addEventListener("click", function () {
+      speedDial.classList.toggle("active");
+    });
+  }
+
+  if (muteBtn) {
+    muteBtn.addEventListener("click", function () {
+      if (isMuted) {
+        console.log("Test");
+        window.pepperController.setUnmute();
+        window.pepperController.playSound(0);
+        muteBtn.querySelector("img").src = "./assets/icons/volume_mute.svg";
+      } else {
+        console.log("Test");
+        window.pepperController.setMute();
+        window.pepperController.playSound(0);
+        muteBtn.querySelector("img").src = "./assets/icons/volume_on.png";
+      }
+
+      isMuted = !isMuted;
+    });
+  }
 });
--- html/newDesign/scripts/settings.js
+++ html/newDesign/scripts/settings.js
@@ -1,41 +1,49 @@
 var isMuted = false;
 
 function toggleMute() {
-    if (isMuted) {
-        window.pepperController.setUnmute();
-        window.pepperController.playSound(0);
-        document.getElementById("muteText").textContent = "Stummschalten";
-    } else {
-        window.pepperController.setMute();
-        window.pepperController.playSound(0);
-        document.getElementById("muteText").textContent = "Entstummen";
-    }
+  if (isMuted) {
+    window.pepperController.setUnmute();
+    window.pepperController.playSound(0);
+    document.getElementById("muteText").textContent = "Stummschalten";
+    document.querySelector("#muteBtn img").src =
+      "../assets/icons/volume_mute.svg";
+  } else {
+    window.pepperController.setMute();
+    window.pepperController.playSound(0);
+    document.getElementById("muteText").textContent = "Entstummen";
+    document.querySelector("#muteBtn img").src =
+      "../assets/icons/volume_on.png";
+  }
 
-    isMuted = !isMuted;
+  isMuted = !isMuted;
 }
 
 function volPlus() {
-    window.pepperController.volPlus();
-    window.pepperController.playSound(0);
+  window.pepperController.volPlus();
+  window.pepperController.playSound(0);
 }
 
 function volMinus() {
-    window.pepperController.volMinus();
-    window.pepperController.playSound(0);
+  window.pepperController.volMinus();
+  window.pepperController.playSound(0);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("muteBtn").addEventListener("click", function () {
-        if (isMuted) {
-            window.pepperController.setUnmute();
-            window.pepperController.playSound(0);
-            document.getElementById("muteText").textContent = "Stummschalten";
-        } else {
-            window.pepperController.setMute();
-            window.pepperController.playSound(0);
-            document.getElementById("muteText").textContent = "Entstummen";
-        }
+  document.getElementById("muteBtn").addEventListener("click", function () {
+    if (isMuted) {
+      window.pepperController.setUnmute();
+      window.pepperController.playSound(0);
+      document.getElementById("muteText").textContent = "Stummschalten";
+      document.querySelector("#muteBtn img").src =
+        "../../assets/icons/volume_mute.svg";
+    } else {
+      window.pepperController.setMute();
+      window.pepperController.playSound(0);
+      document.getElementById("muteText").textContent = "Entstummen";
+      document.querySelector("#muteBtn img").src =
+        "../../assets/icons/volume_on.png";
+    }
 
-        isMuted = !isMuted;
-    });
+    isMuted = !isMuted;
+  });
 });
