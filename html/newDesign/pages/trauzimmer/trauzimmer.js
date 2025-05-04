48478e608b3c11c2f6ad11f29aa36bfd0ad3e43b
David Pospisil <pospisil.david.29@gmail.com>
2025-05-04 08:56:29 +0200

    Made trauzimmer webview compatible

--- html/newDesign/pages/trauzimmer/trauzimmer.js
+++ html/newDesign/pages/trauzimmer/trauzimmer.js
@@ -1,29 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Get all location items and details cards
-  const locationItems = document.querySelectorAll(".location-item");
-  const locationCards = document.querySelectorAll(".location-card");
+  var locationItems = document.querySelectorAll(".location-item");
+  var locationCards = document.querySelectorAll(".location-card");
 
   // Add click event listeners to location items
-  locationItems.forEach((item) => {
-    item.addEventListener("click", function () {
+  for (var i = 0; i < locationItems.length; i++) {
+    locationItems[i].addEventListener("click", function () {
       // Remove active class from all items
-      locationItems.forEach((location) => location.classList.remove("active"));
+      for (var j = 0; j < locationItems.length; j++) {
+        locationItems[j].classList.remove("active");
+      }
 
       // Add active class to clicked item
       this.classList.add("active");
 
       // Get the location ID and show corresponding details
-      const locationId = this.getAttribute("data-location");
-      locationCards.forEach((card) => (card.style.display = "none"));
-      document.getElementById(`${locationId}-details`).style.display = "block";
+      var locationId = this.getAttribute("data-location");
+      for (var k = 0; k < locationCards.length; k++) {
+        locationCards[k].style.display = "none";
+      }
+      var detailsCard = document.getElementById(locationId + "-details");
+      if (detailsCard) {
+        detailsCard.style.display = "block";
+      }
     });
-  });
+  }
 
   // Initialize - show only the active location
-  locationCards.forEach((card) => (card.style.display = "none"));
-  const activeLocation = document.querySelector(".location-item.active");
+  for (var l = 0; l < locationCards.length; l++) {
+    locationCards[l].style.display = "none";
+  }
+  var activeLocation = document.querySelector(".location-item.active");
   if (activeLocation) {
-    const activeId = activeLocation.getAttribute("data-location");
-    document.getElementById(`${activeId}-details`).style.display = "block";
+    var activeId = activeLocation.getAttribute("data-location");
+    var activeDetailsCard = document.getElementById(activeId + "-details");
+    if (activeDetailsCard) {
+      activeDetailsCard.style.display = "block";
+    }
   }
 });
