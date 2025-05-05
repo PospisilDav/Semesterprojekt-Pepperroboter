580aa34c115fd3f6518b50b8ca04092f89e001e7
manu <133259580+manudeobs@users.noreply.github.com>
2025-05-05 13:05:25 +0200

    fixed screensaver time padding

--- html/newDesign/pages/screenSaver/screenSaver.js
+++ html/newDesign/pages/screenSaver/screenSaver.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     setInterval(function() {
         const date = new Date()
         var hours = ((date.getHours() + 2) % 24).toString()
-        hours = hours < 2 ? "0" + hours : hours
+        hours = hours.length < 2 ? "0" + hours : hours
         var minutes = date.getMinutes().toString()
-        minutes = minutes < 2 ? "0" + minutes : minutes
+        minutes = minutes.length < 2 ? "0" + minutes : minutes
         document.getElementById("clockDisplay").innerText = hours + ":" + minutes
     }, 1000)
 })
