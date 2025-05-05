42612d2d609e6294ae4db11e3e96abb2b59f1b93
manu <133259580+manudeobs@users.noreply.github.com>
2025-05-05 13:03:49 +0200

    screensaver minute padding

--- html/newDesign/pages/screenSaver/screenSaver.js
+++ html/newDesign/pages/screenSaver/screenSaver.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const date = new Date()
         var hours = ((date.getHours() + 2) % 24).toString()
         hours = hours < 2 ? "0" + hours : hours
-        document.getElementById("clockDisplay").innerText = hours + ":" + date.getMinutes()
+        var minutes = date.getMinutes().toString()
+        minutes = minutes < 2 ? "0" + minutes : minutes
+        document.getElementById("clockDisplay").innerText = hours + ":" + minutes
     }, 1000)
 })
