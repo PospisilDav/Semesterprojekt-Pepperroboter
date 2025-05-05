0d0a490a52ec67d400667aeb82480d6dc481c595
manu <133259580+manudeobs@users.noreply.github.com>
2025-05-05 12:51:55 +0200

    updated screensaver

--- html/newDesign/pages/screenSaver/screenSaver.js
+++ html/newDesign/pages/screenSaver/screenSaver.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     setInterval(function() {
         const date = new Date()
-        const hours = date.getHours().length < 2 ? "0" + date.getHours() : date.getHours()
+        var hours = ((date.getHours() + 2) % 24).toString()
+        hours = hours < 2 ? "0" + hours : hours
         document.getElementById("clockDisplay").innerText = hours + ":" + date.getMinutes()
     }, 1000)
 })
